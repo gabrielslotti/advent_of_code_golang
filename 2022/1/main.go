2721c3b1e5d345b00102f31bac935be8e0eb1ec9
Gabriel Lotti <gabriel.lotti@sensedata.com.br>
2023-07-18 09:42:10 -0300

    Day 1 solved

---
+++ 2022/1/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"sort"
+	"bufio"
+	"strconv"
+)
+
+func main() {
+
+    readFile, err := os.Open("input.txt")
+
+    if err != nil {
+        fmt.Println(err)
+    }
+    fileScanner := bufio.NewScanner(readFile)
+    fileScanner.Split(bufio.ScanLines)
+    var fileLines []string
+
+    for fileScanner.Scan() {
+        fileLines = append(fileLines, fileScanner.Text())
+    }
+
+    readFile.Close()
+
+	var greatest_amount int
+	var past_elf int
+	var current_elf int
+	var amount_slice []int
+
+    for _, line := range fileLines {
+		switch line {
+			case "":
+				if current_elf > past_elf && current_elf > greatest_amount {
+					greatest_amount = current_elf
+				}
+
+				amount_slice = append(amount_slice, current_elf)
+
+				past_elf = current_elf
+				current_elf = 0
+				continue
+			default:
+				calories, err := strconv.Atoi(line)
+				if err != nil {
+					panic(err)
+				}
+
+				current_elf = current_elf + calories
+		}
+    }
+
+	sort.Ints(amount_slice)
+	top3 := amount_slice[len(amount_slice)-3:]
+
+	var total int
+	for _, value := range top3 {
+		total = total + value	
+  	}
+
+	fmt.Println("Answer:", total, "calories.")
+}
+
